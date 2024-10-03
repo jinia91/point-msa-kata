8d94a775d2fbaa4a625826008ec6b828dc033db1
jinia91 <creee77@gmail.com>
2024-10-03 21:40:12 +0900

    init

---
+++ src/main/kotlin/com/msa/pointmsakata/PointMsaKataApplication.kt
@@ -0,0 +1,11 @@
+package com.msa.pointmsakata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointMsaKataApplication
+
+fun main(args: Array<String>) {
+    runApplication<PointMsaKataApplication>(*args)
+}
